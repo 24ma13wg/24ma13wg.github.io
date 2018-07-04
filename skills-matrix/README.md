c79065777e2fcc7a83b0d78f7087878595a25dce
John Durance <john.durance@wunder.io>
2018-07-04 21:32:35 +0100

    .

---
+++ skills-matrix/README.md
@@ -0,0 +1,14 @@
+# Skills matrix
+## Possible user stories
+* As a resourcer, I want to know what skills we are short of so that I can recruit new people who have those skills
+* As a resourcer, I want to know what skills we are short of, and who wants to increase a certain skill, so that I can organise appropriate training
+* As a resourcer I wan to know who is available to coach others in a certain skill
+* As a delivery manager, I want to know who has a skill, at a level that can deliver a project, so that I can select the right people for my project
+* As a delivery manager, I want to know who wants to increase a certain skill, so that they can be paired with more skilled people on projects
+* As a developer, I want to know what skills are strategically important to the company, and where there are deficiencies, so that I can keep future personal development in close alignment
+## Possible form collection
+I think that grading skill level, either by self assessment and/or by peer review, is problematic. Binary questions are better:
+* Do you have any x skills: yes/no
+* Do you feel confident applying skill x to a client project: yes/no
+* Do you feel confident to teach/coach skill x to others: yes/no
+* Is it a priority for you to become more skilled at x: yes/no
---
+++ skills-matrix/chart.js
@@ -0,0 +1,164 @@
+// Globals.
+const radius = 8;
+let count = 0;
+const column = 150;
+const kerning = 5;
+
+// Add SVG container.
+const svg = d3.select("body")
+  .append("svg")
+  .attr("width", 960)
+  .attr("height", 480);
+
+// Add legend
+const legend = svg.append("g")
+  .attr("class", "legend");
+legend.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 478)
+  .attr("height", 80)
+  .attr("class", "legend-container");
+
+// Add basic skills key.
+const basic = legend.append("g")
+  .attr("transform", "translate(0, 50)");
+basic.append("circle")
+  .attr("r", radius)
+  .attr("cx", 56)
+  .attr("cy", -25)
+  .attr("class", "basic");
+basic.append("text")
+  .attr("x", 17)
+  .text("Basic skills");
+
+// Add project capable key.
+const project = legend.append("g")
+  .attr("transform", "translate(120, 50)");
+project.append("circle")
+  .attr("r", radius)
+  .attr("cx", 56)
+  .attr("cy", -25)
+  .attr("class", "project");
+project.append("text")
+  .attr("x", 0)
+  .text("Project capable");
+
+// Add student key.
+const student = legend.append("g")
+  .attr("transform", "translate(240, 50)");
+student.append("circle")
+  .attr("r", radius)
+  .attr("cx", 56)
+  .attr("cy", -25)
+  .attr("class", "student");
+student.append("text")
+  .attr("x", 28)
+  .text("Student");
+
+// Add teacher key.
+const teacher = legend.append("g")
+  .attr("transform", "translate(360, 50)");
+teacher.append("circle")
+  .attr("r", radius)
+  .attr("cx", 56)
+  .attr("cy", -25)
+  .attr("class", "teacher");
+teacher.append("text")
+  .attr("x", 27)
+  .text("Teacher");
+
+// Load data.
+d3.json("data.json").then(function(data) {
+
+  // Nest data by category and by skill, and order everything alphabetcally.
+  const nest = d3.nest()
+    .key(function(d){
+      return d.category;
+    })
+    .sortKeys(d3.ascending)
+    .key(function(d){
+      return d.skill;
+    })
+    .sortKeys(d3.ascending)
+    .key(function(d){
+      return d.name;
+    })
+    .sortKeys(d3.ascending)
+    .entries(data);
+
+  // Generate array of header labels.
+  const headerLabels = nest.map(d => d.key);
+
+  // Define scales with a range of categorical colours.
+  const light = d3.scaleOrdinal(d3.schemePastel2)
+    .domain(headerLabels);
+  const dark = d3.scaleOrdinal(d3.schemeDark2)
+    .domain(headerLabels);
+
+  // Add category groups.
+  const categories = svg.selectAll(".category")
+    .data(nest)
+    .enter()
+    .append("g")
+    .attr("transform", "translate(0, 120)")
+    .attr("class", "category");
+
+  // Add category headers.
+  categories.selectAll(".header")
+    .data(d => d.values)
+    .enter()
+    .append("text")
+    .attr("transform", () => "translate(0, " + ++count * 32 + ")")
+    .attr("class", (d, i) => i ? "empty" : "header")
+    .text((d, i) => i ? "" : headerLabels.shift());
+  d3.selectAll(".empty").remove();
+
+  // Add skill groups.
+  count = 0;
+  const skills = categories.selectAll(".skill")
+    .data(d => d.values)
+    .enter()
+    .append("g")
+    .attr("transform", () => "translate(" + column + ", " + ++count * 32 + ")")
+    .attr("class", "skill");
+  skills.append("text")
+    .text(d => d.key);
+
+  // Add skilled people.
+  const people = skills.selectAll(".person")
+    .data(d => d.values)
+    .enter()
+    .append("g")
+    .attr("class", "person");
+  people.append("circle")
+    .attr("r", radius)
+    .attr("cx", (d, i) => {
+      return i * ((radius * 2) + kerning) + column;
+    })
+    .attr("cy", 0 - (radius / 2))
+    .attr("fill", d => {
+      let fill = light(d.values[0].category);
+      if (d.values[0].project) {
+        fill = dark(d.values[0].category);
+      }
+      if (d.values[0].teacher) {
+        fill = "#555";
+      }
+      return fill;
+    })
+    .attr("stroke", d => {
+      let stroke = light(d.values[0].category);
+      if (d.values[0].project) {
+        stroke = dark(d.values[0].category);
+      }
+      if (d.values[0].student) {
+        stroke = "#555";
+      }
+      return stroke;
+    })
+    .attr("stroke-width", 3);
+  people.append("title")
+    .text(d => d.key);
+
+});
