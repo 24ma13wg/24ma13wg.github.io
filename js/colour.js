debc0b38bf3535b9c6a34e6c6e645f1e5c38f4c7
John Durance <john.durance@wunder.io>
2018-10-03 07:41:43 +0100

    .

---
+++ js/colour.js
@@ -0,0 +1,3 @@
+const colours = ["#e5d7ac", "#bcd8c5", "#dbeace", "#e4c3b4", "#eaedef"];
+const random = colours[Math.floor(Math.random() * colours.length)];
+document.body.style.background = random;
