ec9e9bde56892fc5acf315056d0d577abd0484ed
John Durance <24ma13wg@gmail.com>
2018-10-13 11:20:29 +0100

    .

--- js/colour.js
+++
@@ -1,3 +0,0 @@
-const colours = ["#e5d7ac", "#bcd8c5", "#dbeace", "#e7dbce", "#eaedef"];
-const random = colours[Math.floor(Math.random() * colours.length)];
-document.body.style.background = random;
